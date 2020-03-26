983eb4bebdbcd492702a0abd79d594fe95739ee1
BritishPandaChick <sbartley1080@gmail.com>
2020-03-25 21:02:55 -0400

    Add position property to magnify class. Add small selector to CSS. Create if-statements to fade in and out if mouse is outside the container

--- js/script.js
+++ js/script.js
@@ -25,21 +25,34 @@ $(document).ready(function(){
         //We will deduct the positions of .magnify from the mouse positions with 
         //respect to the document to get the mouse positions with respect to the  
         //container(.magnify)
-        var mouse_x = e.pageX - magnify_offset.left;
-        var mouse_y = e.pageY - magnify_offset.left;
+        var mx = e.pageX - magnify_offset.left;
+        var my = e.pageY - magnify_offset.top;
 
-        //The background position of .large will be changed according to the position 
-        //of the mouse over the .small image. So we will get the ratio of the pixel 
-        //under the mouse pointer with respect to the image and use that to position the 
-        //larger image inside the magnifying glass 
-        var ratio_x = mouse_x/$(".small").width();
-        //Corresponding pixel on the large image 
-        var large_ratio_x = ratio_x*native_width;
-        //Centering the background inside the glass 
-        large_x = large_x - $(".large").width()/2;
-        //Because the background position will be specified in pixels, we need to 
-        //turn it negative to make it work 
-        large_x = large_x * -1;
-        //Lets combine the entire calculation now 
+        //Finally the code to fade out the class if the mouse is outside the container 
+        if(mx <= $(this).width() && my <= $(this).height() && mx >= 0 && my >= 0){
+            $(".large").fadeIn(100);
+        } else {
+            $(".large").fadeOut(100);
+        }
+
+        if($(".large").is(":visible")){
+            //The background position of .large will be changed according to the position 
+            //of the mouse over the .small image. So we will get the ratio of the pixel 
+            //under the mouse pointer with respect to the image and use that to position the 
+            //larger image inside the magnifying glass 
+            var rx = Math.round(mx/$(".small").width()*native_width - $(".large").width()/2)*-1;
+            var ry = Math.round(my/$(".small").height()*native_height - $(".large").height()/2)*-1;
+            var bgp = rx + "px" + ry + "px";
+            
+            //Time to move the magnifying glass with the mouse 
+            var px = mx - $(".large").width()/2;
+            var py = my - $(".large").height()/2;
+            //Now the glass moves with the mouse 
+            //The logic is to deduct half of the glass's width and height from th 
+            //mouse coordinates to place it with its center at the mouse coordinates 
+
+            //If you hover on the image now, you should see the magnifying class in action 
+            $(".large").css({left: px, top: py, backgroundPosition; bgp});
+        }
     })
 });
