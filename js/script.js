a974917289f54bc3a666e269877e2a680666291a
BritishPandaChick <sbartley1080@gmail.com>
2020-03-27 17:42:37 -0400

    Debug code files

--- js/script.js
+++ js/script.js
@@ -1,32 +1,29 @@
 $(document).ready(function(){
+
     var native_width = 0;
     var native_height = 0;
 
     //Now the mousemove function 
     $(".magnify").mousemove(function(e){
-        //When the user hovers on the image, the script will first calculate 
-        //the native dimensions if they don't exist. Only after the native dimensions 
-        //and available, the script will show the zoomed version.
+        /* When the user hovers on the image, the script will first calculate 
+        the native dimensions if they don't exist. Only after the native dimensions 
+        and available, the script will show the zoomed version. */
         if(!native_width && !native_height){
-            //This will create a new image object with the same image as that in .small 
-            //We cannot directly get the dimensions from .small because of the 
-            //width specified to 200px in the html. To get the actual dimensions we have 
-            //created this image object.
+            /* This will create a new image object with the same image as that in .small 
+            We cannot directly get the dimensions from .small because of the 
+            width specified to 200px in the html. To get the actual dimensions we have 
+            created this image object. */
             var image_object = new Image();
-            image_object.src = $(.small).attr("src");
+            image_object.src = $(".small").attr("src");
 
-            //This code is wrapped in the .load function which is important 
-            //width and height of the object would return 0 if accessed before the image gets loaded 
+            /* This code is wrapped in the .load function which is important 
+            width and height of the object would return 0 if accessed before 
+            the image gets loaded */
             native_width = image_object.width;
             native_height = image_object.height;
-
-            //Lets move the mousemove function here inside the image load function 
-            //so that the magnifying glass works only after the image has loaded completely 
-            //Just realised that .load does not trigger for images loading from cache 
-            //in many browsers, hence will use a different technique
         } else {
-            //x/y coordinates of the mouse 
-            //This is the position of .magnify with respect to the document 
+            /* x/y coordinates of the mouse 
+            This is the position of .magnify with respect to the document */
             var magnify_offset = $(this).offset();
             //We will deduct the positions of .magnify from the mouse positions with 
             //respect to the document to get the mouse positions with respect to the  
@@ -35,20 +32,20 @@ $(document).ready(function(){
             var my = e.pageY - magnify_offset.top;
 
             //Finally the code to fade out the class if the mouse is outside the container 
-            if(mx <= $(this).width() && my <= $(this).height() && mx >= 0 && my >= 0){
+            if(mx < $(this).width() && my < $(this).height() && mx > 0 && my > 0){
                 $(".large").fadeIn(100);
             } else {
                 $(".large").fadeOut(100);
             }
 
             if($(".large").is(":visible")){
-                //The background position of .large will be changed according to the position 
-                //of the mouse over the .small image. So we will get the ratio of the pixel 
-                //under the mouse pointer with respect to the image and use that to position the 
-                //larger image inside the magnifying glass 
+                /* The background position of .large will be changed according to the position 
+                of the mouse over the .small image. So we will get the ratio of the pixel 
+                under the mouse pointer with respect to the image and use that to position the 
+                larger image inside the magnifying glass */ 
                 var rx = Math.round(mx/$(".small").width()*native_width - $(".large").width()/2)*-1;
                 var ry = Math.round(my/$(".small").height()*native_height - $(".large").height()/2)*-1;
-                var bgp = rx + "px" + ry + "px";
+                var bgp = rx + "px " + ry + "px";
                 
                 //Time to move the magnifying glass with the mouse 
                 var px = mx - $(".large").width()/2;
@@ -58,7 +55,7 @@ $(document).ready(function(){
                 //mouse coordinates to place it with its center at the mouse coordinates 
 
                 //If you hover on the image now, you should see the magnifying class in action 
-                $(".large").css({left: px, top: py, backgroundPosition; bgp});
+                $(".large").css({left: px, top: py, backgroundPosition: bgp});
             }
         }
     })    
